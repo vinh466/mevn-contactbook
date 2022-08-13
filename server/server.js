b19b377358c90c22e55589b5f562c28d098ee8cb
Vinh Nguyen <vinh466@gmail.com>
2022-08-13 19:03:41 +0700

    feat: set up express

---
+++ server/server.js
@@ -0,0 +1,6 @@
+const app = require('./app')
+const config = require('./app/config');
+
+const PORT = config.app.port;
+
+app.listen(PORT, () => console.log(`Server is running on http://localhost:${PORT}/`))
