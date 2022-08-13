ef74ed254d7b406d70f85e837573d6a26dd0ae7d
Vinh Nguyen <vinh466@gmail.com>
2022-08-13 19:11:44 +0700

    feat: set up mongodb connect

--- server/server.js
+++ server/server.js
@@ -1,6 +1,19 @@
 const app = require('./app')
 const config = require('./app/config');
+const MongoDB = require('./app/utils/mongodb.util');
 
 const PORT = config.app.port;
 
-app.listen(PORT, () => console.log(`Server is running on http://localhost:${PORT}/`))
+async function startServer() {
+    try {
+        await MongoDB.connect(config.db.uri)
+        console.log('Connect to database server  !!');
+        app.listen(PORT, () => console.log(`Server is running on http://localhost:${PORT}/`))
+
+    } catch (error) {
+        console.log(`Cann't connect to database server !!\n`, error);
+        process.exit();
+    }
+}
+
+startServer()
